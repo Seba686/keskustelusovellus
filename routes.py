8c4ae0ec4c6aa67b1d41d943d2f85ce06074bc45
Sebastian Beijar <sebastian.beijar@helsinki.fi>
2024-04-03 15:51:34 +0300

    Refactor: add file posts.py

--- routes.py
+++ routes.py
@@ -2,11 +2,11 @@ from flask import render_template, request, redirect, url_for
 from sqlalchemy.sql import text
 
 from app import app
-from db import db
+import posts
+
 @app.route("/")
 def index():
-    result = db.session.execute(text("SELECT id, title, content, created FROM threads ORDER BY created DESC"))
-    threads = result.fetchall()
+    threads = posts.show_threads()
     return render_template("index.html", threads=threads) 
 
 @app.route("/new")
@@ -17,7 +17,5 @@ def new():
 def send():
     content = request.form["content"]
     title = request.form["title"]
-    sql = text("INSERT INTO threads (title, content, created) VALUES (:title, :content, NOW())")
-    db.session.execute(sql, {"title":title, "content":content})
-    db.session.commit()
+    posts.new_thread(title, content)
     return redirect("/")
